430bad212f7d32adccf89b6bd702577c83ace6e0
Greg London <gregdlondon@gmail.com>
2017-02-05 21:16:28 -0600

    Created template from tunes.php

--- tunes.php
+++ tunes.php
@@ -1,229 +1,248 @@
-<table>
-	<tr>
+<?php
 
-		<td valign="top">
+/*
+* Template Name: Tunes
+*/
 
-			<ul>
+get_header();
+?>
 
-				<li>Ain’t Miss Behavin</li>
-				<li>Afternoon In Paris</li>
-				<li>A Night In Tunisia</li>
-				<li>All Blues</li>
-				<li>All of Me</li>
-				<li>All The Things You Are</li>
-				<li>As Time Goes By</li>
-				<li>Autumn Leaves</li>
-				<li>A Child Is Born</li>
-				<li>A Foggy Day</li>
-				<li>Basin Street Blues</li>
-				<li>Beyond The Sea</li>
-				<li>Black Orpheus</li>
-				<li>Blue Bossa</li>
-				<li>Blue Monk</li>
-				<li>Bill Bailey Won’t You Please..</li>
-				<li>Blueberry Hill</li>
-				<li>Bourbon Street Parade</li>
-				<li>Big Chief</li>
-				<li>Bye Bye Black Bird</li>
-				<li>Dinah</li>
-				<li>Dream a Little Dream of Me</li>
-				<li>Bye Bye Blackbird</li>
-				<li>Days of Wine and Roses</li>
-				<li>Don’t Get Around Much..</li>
-				<li>Do You Know What It Means</li>
-				<li>Dream a Little Dream of Me</li>
-				<li>Exactly Like You</li>
-				<li>Feel Like Makin Love</li>
-				<li>Fly Me To The Moon</li>
-				<li>Georgia</li>
-				<li>Girl From Ipanema</li>
-				<li>God Bless The Child</li>
-				<li>Grazin In The Grass</li>
-				<li>Groove Merchant</li>
-				<li>Hallelujah</li>
-				<li>Here There, Everywhere</li>
-				<li>Honky Tonk Train Blues</li>
-				<li>How High The Moon</li>
-				<li>I’ll Be There</li>
-				<li>I’m Walkin</li>
-				<li>In a Sentimental Mood</li>
-				<li>In a Mellow Tone</li>
-				<li>In The Summer Time</li>
-			</ul>
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-			<h2>SWING</h2>
+			<h1>Tunes</h1>
 
-			<ul>
-				<li>Ain’t Miss Behavin</li>
-				<li>All of Me</li>
-				<li>All The Things You Are</li>
-				<li>Autumn Leaves</li>
-				<li>A Child Is Born</li>
-				<li>A Foggy Day</li>
-				<li>Dream a Little Dream of Me</li>
-				<li>Bye Bye Blackbird</li>
-				<li>Days of Wine and Roses</li>
-				<li>Don’t Get Around Much..</li>
-				<li>Fly Me To The Moon</li>
-				<li>How High The Moon</li>
-				<li>In a Mellow Tone</li>
-				<li>It Don’t Mean a Thing..</li>
-				<li>My Favorite Things</li>
-				<li>My Romance</li>
-			</ul>
+			<table>
+				<tr>
 
-			
+					<td valign="top">
 
-		</td>
-		<td valign="top">
-			<h2>NEW ORLEANS</h2>
+						<ul>
 
-			<ul>
-				<li>Basin Street Blues</li>
-				<li>Big Chief</li>
-				<li>Bill Bailey</li>
-				<li>Blueberry Hill</li>
-				<li>Bourbon Street Parade</li>
-				<li>Dinah</li>
-				<li>Do You Know What It Means</li>
-				<li>Georgia</li>
-				<li>Grazin In The Grass</li>
-				<li>In The Summer Time</li>
-				<li>Iko Iko</li>
-				<li>Oh When The Saints</li>
-				<li>On The Sunny Side of The Street</li>
-				<li>Sheik of Araby</li>
-				<li>St. James Infirmary</li>
-				<li>Summertime</li>
-				<li>Sweet Georgia Brown</li>
-				<li>Sweet Lorraine</li>
-				<li>They All Asked For</li>
-				<li>What a Wonderful World</li>
-				<li>Walkin To New Orleans</li>
-				<li>When Your’e Smilin</li>
-				<li>Second Line</li>
-				<li>Iko Iko</li>
-				<li>Isn’t She Lovely</li>
-				<li>It Don’t Mean a Thing..</li>
-				<li>It Was a Very Good Year</li>
-				<li>Just the Two of Us</li>
-				<li>Killing Me Softly</li>
-				<li>Let’s Stay Together</li>
-				<li>Masquerade (This)</li>
-				<li>Meditation</li>
-				<li>Mercy,Mercy</li>
-				<li>Misty</li>
-				<li>More</li>
-				<li>Mr. Magic</li>
-				<li>Mr. PC</li>
-				<li>My Foolish Heart</li>
-				<li>My Funny Valentine</li>
-				<li>My Favorite Things</li>
-				<li>My Romance</li>
-				<li>My Way</li>
-				<li>New York, New York</li>
-				<li>Oh When The Saints</li>
-				<li>OnThe Sunny Side of The Street</li>
-				<li>O Sole’ Mio</li>
-				<li>Satin Doll</li>
-				<li>Sheik of Araby</li>
-				<li>Second Line</li>
-				<li>Song For My Father</li>
-				<li>Stagger Lee</li>
-				<li>St. James Infirmary</li>
-				<li>Summertime</li>
-				<li>St. Thomas</li>
-				<li>Stolen Moments</li>
-				<li>Sugar</li>
-				<li>Sweet Georgia Brown</li>
-				<li>Sweet Lorraine</li>
-				<li>Tangerine</li>
-				<li>Take 5</li>
-				<li>Take The “A” Train</li>
-				<li>There Is No Greater Love </li>
-				<li>They All Asked For You</li>
-				<li>The Way You Look Tonight</li>
-				<li>There Will Never B Another U</li>
-				<li>Satin Doll</li>
-				<li>St. Thomas</li>
-				<li>Stolen Moments</li>
-				<li>Take 5</li>
-				<li>There Is No Greater Love You</li>
-				<li>The Way You Look Tonight</li>
+							<li>Ain’t Miss Behavin</li>
+							<li>Afternoon In Paris</li>
+							<li>A Night In Tunisia</li>
+							<li>All Blues</li>
+							<li>All of Me</li>
+							<li>All The Things You Are</li>
+							<li>As Time Goes By</li>
+							<li>Autumn Leaves</li>
+							<li>A Child Is Born</li>
+							<li>A Foggy Day</li>
+							<li>Basin Street Blues</li>
+							<li>Beyond The Sea</li>
+							<li>Black Orpheus</li>
+							<li>Blue Bossa</li>
+							<li>Blue Monk</li>
+							<li>Bill Bailey Won’t You Please..</li>
+							<li>Blueberry Hill</li>
+							<li>Bourbon Street Parade</li>
+							<li>Big Chief</li>
+							<li>Bye Bye Black Bird</li>
+							<li>Dinah</li>
+							<li>Dream a Little Dream of Me</li>
+							<li>Bye Bye Blackbird</li>
+							<li>Days of Wine and Roses</li>
+							<li>Don’t Get Around Much..</li>
+							<li>Do You Know What It Means</li>
+							<li>Dream a Little Dream of Me</li>
+							<li>Exactly Like You</li>
+							<li>Feel Like Makin Love</li>
+							<li>Fly Me To The Moon</li>
+							<li>Georgia</li>
+							<li>Girl From Ipanema</li>
+							<li>God Bless The Child</li>
+							<li>Grazin In The Grass</li>
+							<li>Groove Merchant</li>
+							<li>Hallelujah</li>
+							<li>Here There, Everywhere</li>
+							<li>Honky Tonk Train Blues</li>
+							<li>How High The Moon</li>
+							<li>I’ll Be There</li>
+							<li>I’m Walkin</li>
+							<li>In a Sentimental Mood</li>
+							<li>In a Mellow Tone</li>
+							<li>In The Summer Time</li>
+						</ul>
 
-			</ul>
+						<h2>SWING</h2>
 
-			<h2>BLUES</h2>
+						<ul>
+							<li>Ain’t Miss Behavin</li>
+							<li>All of Me</li>
+							<li>All The Things You Are</li>
+							<li>Autumn Leaves</li>
+							<li>A Child Is Born</li>
+							<li>A Foggy Day</li>
+							<li>Dream a Little Dream of Me</li>
+							<li>Bye Bye Blackbird</li>
+							<li>Days of Wine and Roses</li>
+							<li>Don’t Get Around Much..</li>
+							<li>Fly Me To The Moon</li>
+							<li>How High The Moon</li>
+							<li>In a Mellow Tone</li>
+							<li>It Don’t Mean a Thing..</li>
+							<li>My Favorite Things</li>
+							<li>My Romance</li>
+						</ul>
 
-			<ul>
+						
 
-				<li>All Blues</li>
-				<li>Blue Monk</li>
-				<li>Freddie The Free Loader</li>
-				<li>Mercy, Mercy</li>
-				<li>Mr. PC</li>
-				<li>Willow Weep For Me </li>
+					</td>
+					<td valign="top">
+						<h2>NEW ORLEANS</h2>
 
-			</ul>
-			
-		
+						<ul>
+							<li>Basin Street Blues</li>
+							<li>Big Chief</li>
+							<li>Bill Bailey</li>
+							<li>Blueberry Hill</li>
+							<li>Bourbon Street Parade</li>
+							<li>Dinah</li>
+							<li>Do You Know What It Means</li>
+							<li>Georgia</li>
+							<li>Grazin In The Grass</li>
+							<li>In The Summer Time</li>
+							<li>Iko Iko</li>
+							<li>Oh When The Saints</li>
+							<li>On The Sunny Side of The Street</li>
+							<li>Sheik of Araby</li>
+							<li>St. James Infirmary</li>
+							<li>Summertime</li>
+							<li>Sweet Georgia Brown</li>
+							<li>Sweet Lorraine</li>
+							<li>They All Asked For</li>
+							<li>What a Wonderful World</li>
+							<li>Walkin To New Orleans</li>
+							<li>When Your’e Smilin</li>
+							<li>Second Line</li>
+							<li>Iko Iko</li>
+							<li>Isn’t She Lovely</li>
+							<li>It Don’t Mean a Thing..</li>
+							<li>It Was a Very Good Year</li>
+							<li>Just the Two of Us</li>
+							<li>Killing Me Softly</li>
+							<li>Let’s Stay Together</li>
+							<li>Masquerade (This)</li>
+							<li>Meditation</li>
+							<li>Mercy,Mercy</li>
+							<li>Misty</li>
+							<li>More</li>
+							<li>Mr. Magic</li>
+							<li>Mr. PC</li>
+							<li>My Foolish Heart</li>
+							<li>My Funny Valentine</li>
+							<li>My Favorite Things</li>
+							<li>My Romance</li>
+							<li>My Way</li>
+							<li>New York, New York</li>
+							<li>Oh When The Saints</li>
+							<li>OnThe Sunny Side of The Street</li>
+							<li>O Sole’ Mio</li>
+							<li>Satin Doll</li>
+							<li>Sheik of Araby</li>
+							<li>Second Line</li>
+							<li>Song For My Father</li>
+							<li>Stagger Lee</li>
+							<li>St. James Infirmary</li>
+							<li>Summertime</li>
+							<li>St. Thomas</li>
+							<li>Stolen Moments</li>
+							<li>Sugar</li>
+							<li>Sweet Georgia Brown</li>
+							<li>Sweet Lorraine</li>
+							<li>Tangerine</li>
+							<li>Take 5</li>
+							<li>Take The “A” Train</li>
+							<li>There Is No Greater Love </li>
+							<li>They All Asked For You</li>
+							<li>The Way You Look Tonight</li>
+							<li>There Will Never B Another U</li>
+							<li>Satin Doll</li>
+							<li>St. Thomas</li>
+							<li>Stolen Moments</li>
+							<li>Take 5</li>
+							<li>There Is No Greater Love You</li>
+							<li>The Way You Look Tonight</li>
 
-		</td>
-		
-		<td valign="top">
+						</ul>
 
-			<h2>BOSSA/LATIN FUNK</h2>
-			<ul>
-				<li>Black Orpheus</li>
-				<li>Blue Bossa</li>
-				<li>Feel Like Makin Love</li>
-				<li>Girl From Ipanema</li>
-				<li>Killing Me Softly</li>
-				<li>Masquerade (This)</li>
-				<li>Meditation</li>
-				<li>Song For My Father</li>
-				<li>Sugar</li>
-				<li>Tipitina</li>
-				<li>Torna Mia a Surrento</li>
-				<li>Unforgettable</li>
-				<li>Watermelon Man</li>
-				<li>What a Wonderful World</li>
-				<li>Walkin To New Orleans</li>
-				<li>When I Fall In Love</li>
-				<li>When Sunny Gets Blue</li>
-				<li>When The Saints Go</li>
-				<li>Marching In</li>
-				<li>When Your’e Smilin</li>
-				<li>Willow Weep For Me</li>
-				<li>Witchcraft</li>
-				<li>Work Song</li>
-				<li>You Are The Sunshine of My..</li>
-			</ul>
+					</td>
+					
+					<td valign="top">
 
-			<h2>BALLAD</h2>
+						<h2>BLUES</h2>
 
-			<ul>
-				<li>As Time Goes By</li>
-				<li>Do You Know What It Means</li>
-				<li>Georgia</li>
-				<li>God Bless The Child</li>
-				<li>Hallelujah</li>
-				<li>Here There, Everywhere</li>
-				<li>I’ll Be There</li>
-				<li>In a Sentimental Mood</li>
-				<li>Misty</li>
-				<li>My Foolish Heart</li>
-				<li>My Funny Valentine</li>
-				<li>Summertime</li>
-				<li>When Sunny Gets Blue</li>
-				<li>Unforgettable</li>
-				<li>Mr. Magic</li>
-				<li>Tangerine</li>
-				<li>Watermelon Man</li>
-				
-			</ul>
+						<ul>
 
-		</td>
+							<li>All Blues</li>
+							<li>Blue Monk</li>
+							<li>Freddie The Free Loader</li>
+							<li>Mercy, Mercy</li>
+							<li>Mr. PC</li>
+							<li>Willow Weep For Me </li>
 
-	</tr>
-</table>
+						</ul>
+
+						<h2>BOSSA/LATIN FUNK</h2>
+						<ul>
+							<li>Black Orpheus</li>
+							<li>Blue Bossa</li>
+							<li>Feel Like Makin Love</li>
+							<li>Girl From Ipanema</li>
+							<li>Killing Me Softly</li>
+							<li>Masquerade (This)</li>
+							<li>Meditation</li>
+							<li>Song For My Father</li>
+							<li>Sugar</li>
+							<li>Tipitina</li>
+							<li>Torna Mia a Surrento</li>
+							<li>Unforgettable</li>
+							<li>Watermelon Man</li>
+							<li>What a Wonderful World</li>
+							<li>Walkin To New Orleans</li>
+							<li>When I Fall In Love</li>
+							<li>When Sunny Gets Blue</li>
+							<li>When The Saints Go</li>
+							<li>Marching In</li>
+							<li>When Your’e Smilin</li>
+							<li>Willow Weep For Me</li>
+							<li>Witchcraft</li>
+							<li>Work Song</li>
+							<li>You Are The Sunshine of My..</li>
+						</ul>
+
+						<h2>BALLAD</h2>
+
+						<ul>
+							<li>As Time Goes By</li>
+							<li>Do You Know What It Means</li>
+							<li>Georgia</li>
+							<li>God Bless The Child</li>
+							<li>Hallelujah</li>
+							<li>Here There, Everywhere</li>
+							<li>I’ll Be There</li>
+							<li>In a Sentimental Mood</li>
+							<li>Misty</li>
+							<li>My Foolish Heart</li>
+							<li>My Funny Valentine</li>
+							<li>Summertime</li>
+							<li>When Sunny Gets Blue</li>
+							<li>Unforgettable</li>
+							<li>Mr. Magic</li>
+							<li>Tangerine</li>
+							<li>Watermelon Man</li>
+							
+						</ul>
+
+					</td>
+
+				</tr>
+			</table>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer(); ?>
