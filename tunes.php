eab97d2a75c1cbf6c9e73e50799999690c0dac9d
Greg London <gregdlondon@gmail.com>
2017-02-05 19:12:54 -0600

    Added a bunch of tunes.

---
+++ tunes.php
@@ -0,0 +1,119 @@
+<table>
+	<tr>
+
+		<td>
+
+			<ul>
+
+				<li>Ain’t Miss Behavin</li>
+				<li>Afternoon In Paris</li>
+				<li>A Night In Tunisia</li>
+				<li>All Blues</li>
+				<li>All of Me</li>
+				<li>All The Things You Are</li>
+				<li>As Time Goes By</li>
+				<li>Autumn Leaves</li>
+				<li>A Child Is Born</li>
+				<li>A Foggy Day</li>
+				<li>Basin Street Blues</li>
+				<li>Beyond The Sea</li>
+				<li>Black Orpheus</li>
+				<li>Blue Bossa</li>
+				<li>Blue Monk</li>
+				<li>Bill Bailey Won’t You Please..</li>
+				<li>Blueberry Hill</li>
+				<li>Bourbon Street Parade</li>
+				<li>Big Chief</li>
+				<li>Bye Bye Black Bird</li>
+				<li>Dinah</li>
+				<li>Dream a Little Dream of Me</li>
+				<li>Bye Bye Blackbird</li>
+				<li>Days of Wine and Roses</li>
+				<li>Don’t Get Around Much..</li>
+				<li>Do You Know What It Means</li>
+				<li>Dream a Little Dream of Me</li>
+				<li>Exactly Like You</li>
+				<li>Feel Like Makin Love</li>
+				<li>Fly Me To The Moon</li>
+				<li>Georgia</li>
+				<li>Girl From Ipanema</li>
+				<li>God Bless The Child</li>
+				<li>Grazin In The Grass</li>
+				<li>Groove Merchant</li>
+				<li>Hallelujah</li>
+				<li>Here There, Everywhere</li>
+				<li>Honky Tonk Train Blues</li>
+				<li>How High The Moon</li>
+				<li>I’ll Be There</li>
+				<li>I’m Walkin</li>
+				<li>In a Sentimental Mood</li>
+				<li>In a Mellow Tone</li>
+				<li>In The Summer Time</li>
+			</ul>
+
+			<h2>SWING</h2>
+
+			<ul>
+				<li>Ain’t Miss Behavin</li>
+				<li>All of Me</li>
+				<li>All The Things You Are</li>
+				<li>Autumn Leaves</li>
+				<li>A Child Is Born</li>
+				<li>A Foggy Day</li>
+				<li>Dream a Little Dream of Me</li>
+				<li>Bye Bye Blackbird</li>
+				<li>Days of Wine and Roses</li>
+				<li>Don’t Get Around Much..</li>
+				<li>Fly Me To The Moon</li>
+				<li>How High The Moon</li>
+				<li>In a Mellow Tone</li>
+				<li>It Don’t Mean a Thing..</li>
+				<li>My Favorite Things</li>
+				<li>My Romance</li>
+			</ul>
+
+			<h2>NEW ORLEANS</h2>
+
+			<ul>
+				<li>Basin Street Blues</li>
+				<li>Big Chief</li>
+				<li>Bill Bailey</li>
+				<li>Blueberry Hill</li>
+				<li>Bourbon Street Parade</li>
+				<li>Dinah</li>
+				<li>Do You Know What It Means</li>
+				<li>Georgia</li>
+				<li>Grazin In The Grass</li>
+				<li>In The Summer Time</li>
+				<li>Iko Iko</li>
+				<li>Oh When The Saints</li>
+				<li>On The Sunny Side of The Street</li>
+				<li>Sheik of Araby</li>
+				<li>St. James Infirmary</li>
+				<li>Summertime</li>
+				<li>Sweet Georgia Brown</li>
+				<li>Sweet Lorraine</li>
+				<li>They All Asked For</li>
+				<li>What a Wonderful World</li>
+				<li>Walkin To New Orleans</li>
+				<li>When Your’e Smilin</li>
+				<li>Second Line</li>
+				<li></li>
+
+
+
+
+			</ul>
+
+		</td>
+
+		<td>
+
+		</td>
+
+		<td>
+
+		</td>
+
+	</tr>
+</table>
