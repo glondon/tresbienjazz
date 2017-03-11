68f43937b8be3a762ce841621232373744171585
Greg London <gregdlondon@gmail.com>
2017-03-11 13:03:29 -0600

    Created contemporary array.

--- tunes.php
+++ tunes.php
@@ -11,7 +11,14 @@ $nola_music = array('Basin Street Blues','Big Chief','Bill Bailey','Blueberry Hi
 					'Tipitina');
 sort($nola_music);
 
+$contemporary = array('Across The Universe','Alison','Brown Eyed Girl','Careless Whisper','Change The World','Closing Time','Don’t Know Why',
+					  'Drift Away','Have a Little Faith In Me','Hello It’s Me','Hey There Delilah','Ho, Hey','Home','I’ll Be','New York State of Mind',
+					  'Open Arms','One Love','Piano Man','Sara Smile','She Talks To Angels','The Long and Winding Road','You’ve Got A Friend',
+					  'Your Song');
+sort($contemporary);
+
 get_header();
+
 ?>
 
 <div class="wrap">
@@ -25,76 +32,18 @@ get_header();
 
 					<td valign="top">
 
-						<ul>
-
-							<li>Ain’t Miss Behavin</li>
-							<li>Afternoon In Paris</li>
-							<li>A Night In Tunisia</li>
-							<li>All Blues</li>
-							<li>All of Me</li>
-							<li>All The Things You Are</li>
-							<li>As Time Goes By</li>
-							<li>Autumn Leaves</li>
-							<li>A Child Is Born</li>
-							<li>A Foggy Day</li>
-							<li>Basin Street Blues</li>
-							<li>Beyond The Sea</li>
-							<li>Black Orpheus</li>
-							<li>Blue Bossa</li>
-							<li>Blue Monk</li>
-							<li>Bill Bailey Won’t You Please..</li>
-							<li>Blueberry Hill</li>
-							<li>Bourbon Street Parade</li>
-							<li>Big Chief</li>
-							<li>Bye Bye Black Bird</li>
-							<li>Dinah</li>
-							<li>Dream a Little Dream of Me</li>
-							<li>Bye Bye Blackbird</li>
-							<li>Days of Wine and Roses</li>
-							<li>Don’t Get Around Much..</li>
-							<li>Do You Know What It Means</li>
-							<li>Dream a Little Dream of Me</li>
-							<li>Exactly Like You</li>
-							<li>Feel Like Makin Love</li>
-							<li>Fly Me To The Moon</li>
-							<li>Georgia</li>
-							<li>Girl From Ipanema</li>
-							<li>God Bless The Child</li>
-							<li>Grazin In The Grass</li>
-							<li>Groove Merchant</li>
-							<li>Hallelujah</li>
-							<li>Here There, Everywhere</li>
-							<li>Honky Tonk Train Blues</li>
-							<li>How High The Moon</li>
-							<li>I’ll Be There</li>
-							<li>I’m Walkin</li>
-							<li>In a Sentimental Mood</li>
-							<li>In a Mellow Tone</li>
-							<li>In The Summer Time</li>
-						</ul>
-
-						<h2>SWING</h2>
+						<h2>NEW ORLEANS MUSIC</h2>
 
 						<ul>
-							<li>Ain’t Miss Behavin</li>
-							<li>All of Me</li>
-							<li>All The Things You Are</li>
-							<li>Autumn Leaves</li>
-							<li>A Child Is Born</li>
-							<li>A Foggy Day</li>
-							<li>Dream a Little Dream of Me</li>
-							<li>Bye Bye Blackbird</li>
-							<li>Days of Wine and Roses</li>
-							<li>Don’t Get Around Much..</li>
-							<li>Fly Me To The Moon</li>
-							<li>How High The Moon</li>
-							<li>In a Mellow Tone</li>
-							<li>It Don’t Mean a Thing..</li>
-							<li>My Favorite Things</li>
-							<li>My Romance</li>
+							<?php foreach($nola_music as $value) echo '<li>'.$value.'</li>'; ?>
 						</ul>
 
-						
+						<h2>CONTEMPORARY MUSIC</h2>
+
+						<ul>
+							<?php foreach($contemporary as $value) echo '<li>'.$value.'</li>'; ?>
+						</ul>
+			
 
 					</td>
 					<td valign="top">
