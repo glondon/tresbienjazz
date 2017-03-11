5476c2571025994c44239ad3a80befd32114f587
Greg London <gregdlondon@gmail.com>
2017-03-11 14:06:06 -0600

    Updated footer content.

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -10,5 +10,6 @@
 
 ?>
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'By %s', 'twentyseventeen' ), 'WP' ); ?></a> | 
+	Contact: <a href="tel:9013387988">901-338-7988</a> 
 </div><!-- .site-info -->
